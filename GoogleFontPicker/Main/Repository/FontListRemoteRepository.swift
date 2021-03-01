449f067a0e1ab22e3e00ba9c0eb4f9db6341cd44
ohlulu <z30262226@gmail.com>
2021-03-01 15:58:04 +0800

    [ Update ] : FontListRemoteRepository

--- GoogleFontPicker/Main/Repository/FontListRemoteRepository.swift
+++ GoogleFontPicker/Main/Repository/FontListRemoteRepository.swift
@@ -12,8 +12,22 @@ protocol FontListRemoteRepositorySpec {
 }
 
 final class FontListRemoteRepository: FontListRemoteRepositorySpec {
+    
+    let service: NetworkServiceSpec
+    
+    init(service: NetworkServiceSpec = NetworkService.default) {
+        self.service = service
+    }
 
     func fetch(_ completion: @escaping (Result<FontListEntity, Error>) -> Void) {
-        
+        let request = FontListRequest()
+        service.send(request) { result in
+            switch result {
+            case .success(let entity):
+                completion(.success(entity))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
     }
 }
