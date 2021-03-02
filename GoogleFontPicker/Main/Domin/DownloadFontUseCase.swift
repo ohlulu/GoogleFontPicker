3d75fa1e58eee9185cfd25b18f4d2ca956678431
ohlulu <z30262226@gmail.com>
2021-03-02 21:20:20 +0800

    [ Modify ] : DownloadFontUseCase register font if success

--- GoogleFontPicker/Main/Domin/DownloadFontUseCase.swift
+++ GoogleFontPicker/Main/Domin/DownloadFontUseCase.swift
@@ -10,15 +10,35 @@ import Foundation
 final class DownloadFontUseCase {
     
     let repository: FontRepositorySpec
+    let storage: FontDataStorage
     
-    init(repository: FontRepositorySpec) {
+    init(
+        repository: FontRepositorySpec,
+        storage: FontDataStorage
+    ) {
         self.repository = repository
+        self.storage = storage
     }
 }
 
 extension DownloadFontUseCase {
     
-    func download(url: URL, completion: @escaping (Data) -> Void) {
-        repository.download(url: url, completion: completion)
+    func downloadFont(name: String, url: URL, completion: @escaping (Result<Void, Error>) -> Void) {
+        repository.download(url: url) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let data):
+                self.storage.save(fontName: name, withFontData: data) { result in
+                    switch result {
+                    case .success:
+                        completion(.success(()))
+                    case .failure(let error):
+                        completion(.failure(error))
+                    }
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
     }
 }
