c49234477e537dc03877f0c0126833fbcda5d9d1
ohlulu <z30262226@gmail.com>
2021-03-02 20:55:04 +0800

    [ Modify ] : implement FontRepository with spec. And add DownloadFontRequest

--- GoogleFontPicker/Main/Repository/FontRepository.swift
+++ GoogleFontPicker/Main/Repository/FontRepository.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 protocol FontRepositorySpec {
-    func download(url: URL, completion: @escaping (Data) -> Void)
+    func download(url: URL, completion: @escaping (Result<Data, Error>) -> Void)
 }
 
 final class FontRepository {
@@ -22,7 +22,37 @@ final class FontRepository {
 
 extension FontRepository: FontRepositorySpec {
     
-    func download(url: URL, completion: @escaping (Data) -> Void) {
-        
+    func download(url: URL, completion: @escaping (Result<Data, Error>) -> Void) {
+        let request = DownloadFontRequest(url: url)
+        service.send(request) { result in
+            switch result {
+            case .success(let data):
+                completion(.success(data))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
+
+struct DownloadFontRequest: NetworkRequest {
+
+    typealias Entity = Data
+
+    var baseURL: URL
+    let path: String = ""
+    let method: HTTPMethod = .get
+    let task: NetworkTask = .simple
+    
+    var decisions: [NetworkDecision] {
+        return [
+            StatusCodeDecision(valid: 200 ..< 399),
+            RetryDecision(retryCount: 2),
+            DataTypeTerminatorDecision()
+        ]
+    }
+    
+    init(url: URL) {
+        self.baseURL = url
     }
 }
