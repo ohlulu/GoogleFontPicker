acb718213a10aafe246dfa37b90ee9de319b3285
ohlulu <z30262226@gmail.com>
2021-03-06 13:31:49 +0800

    [ Remove ] : pod

--- Pods/Target Support Files/Pods-GoogleFontPicker-GoogleFontPickerUITests/Pods-GoogleFontPicker-GoogleFontPickerUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GoogleFontPicker_GoogleFontPickerUITests : NSObject
-@end
-@implementation PodsDummy_Pods_GoogleFontPicker_GoogleFontPickerUITests
-@end
--- Pods/Target Support Files/Pods-GoogleFontPickerTests/Pods-GoogleFontPickerTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GoogleFontPickerTests : NSObject
-@end
-@implementation PodsDummy_Pods_GoogleFontPickerTests
-@end
